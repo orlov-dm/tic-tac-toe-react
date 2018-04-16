523226f5a033826a128f66a1b73371210dca249f
Dmitriy Orlov <dm.ed.orlov@gmail.com>
2018-04-16 17:46:12 +0300

    feat: Constants class

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,2 +1,13 @@
 const X_ELEMENT = 'X';
 const O_ELEMENT = 'O';
+
+class Constants {
+    static get X_ELEMENT() {
+        return X_ELEMENT;
+    }
+    static get O_ELEMENT() {
+        return O_ELEMENT;
+    }
+}
+
+export default Constants;
