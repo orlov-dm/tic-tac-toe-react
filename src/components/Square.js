29312ad423306881367b8b7aa1f5f05205edad41
Dmitriy Orlov <dm.ed.orlov@gmail.com>
2018-04-20 20:14:00 +0300

    del: Usage of React Transition Group

--- src/components/Square.js
+++ src/components/Square.js
@@ -1,27 +1,19 @@
 import React, { Component } from 'react';
-import { CSSTransition } from 'react-transition-group';
-import Constants from '../constants/constants';
 
-class Square extends Component {    
+class Square extends Component {
     render() {
         let classNames = ["square"];
         const isWinner = this.props.isWinner;
-        if(isWinner) {
+        if (isWinner) {
             classNames.push("win");
         }
-        if(this.props.value) {
+        if (this.props.value) {
             classNames.push(this.props.value);
         }
-        return <span key={0} className={classNames.join(" ")} onClick={this.props.onClick}>
-            <CSSTransition
-                in={this.props.value === Constants.X_ELEMENT || this.props.value === Constants.O_ELEMENT}
-                timeout={500}
-                classNames={"square-value"}
-                unmountOnExit            
-            >
-                <p>{this.props.value}</p>
-            </CSSTransition>            
-        </span>            
+        const content = this.props.value ? <span>{this.props.value}</span> : "";
+        return <span key={0} className={classNames.join(" ")} onClick={this.props.onClick}>            
+            {content}
+        </span>
     }
 }
 
